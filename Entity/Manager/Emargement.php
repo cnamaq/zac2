b9cf05c1f55b72bc06c7f55e63de2b31248b0d7d
Denis Fohl <dev@df-info.com>
2016-08-05 16:57:01 +0200

    création Entity\Manager\Emargement pour récupération du programme_regroupement

---
+++ Entity/Manager/Emargement.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fohl
+ * Date: 27/05/16
+ * Time: 11:43
+ */
+
+namespace Zac2\Entity\Manager;
+
+
+use Zac2\Common\DicAware;
+use Zac2\Data\Request\SqlString;
+use Zac2\Filter\Multi\Multi;
+
+class Emargement extends DicAware implements ManagerInterface
+{
+    public function get($entity, Multi $filtre)
+    {
+        $em = $this->getDic()->get('entitymanager.gescicca.requeteur');
+        $dataRequest = new SqlString();
+        $dataRequest->setSql("SELECT e.*, 
+                uo.regroupement_programme_code, uo.regroupement_programme_libelle
+            FROM emargement_aqu as e
+            LEFT JOIN unite_ouverte_aqu as uo ON uo.centre_code = e.centre_code 
+                AND uo.annee = e.annee 
+                AND uo.unite_numero = e.unite_numero
+                AND uo.semestre_code = e.semestre_code
+                AND uo.groupe_code = e.groupe_code
+            WHERE e.annee={$filtre->getCritere('annee')->getValue()}");
+        $em->setDataRequestAdapter($dataRequest);
+
+        return $em->get('\Zac2\Domain\Emargement', $filtre);
+    }
+
+}
