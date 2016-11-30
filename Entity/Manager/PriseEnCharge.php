145efa27af63a74926744f862da5837db2a239dc
Sébastien Roux <sebastien.roux@cnam-aquitaine.fr>
2016-11-30 17:00:44 +0100

    Correction du manager PriseEnCharge pour corriger des données Gescicca erronnées

--- Entity/Manager/PriseEnCharge.php
+++ Entity/Manager/PriseEnCharge.php
@@ -20,27 +20,39 @@ class PriseEnCharge extends DicAware implements ManagerInterface
         $em = $this->getDic()->get('entitymanager.gescicca.requeteur');
         $dataRequest = new SqlString();
         // le filtrage doit être appliqué ici à la main
-        $sql = "SELECT p.*, 
-                i.inscription_date, i.centre_attachement_libelle,
-                iu.inscription_unite_date_creation, iu.modalite, iu.groupe_code, iu.sousgroupe_code,
-                u.unite_code, u.unite_nb_heure, u.unite_ects,
-                uo.regroupement_programme_code, uo.regroupement_programme_libelle,
-                i.formation_numero, i.formation_libelle
-            FROM prise_en_charge_aqu as p
-            LEFT JOIN inscription_aqu as i ON i.centre_code=p.centre_code 
-                AND i.auditeur_numero = p.auditeur_numero 
-                AND i.annee = p.annee
-            LEFT JOIN inscription_unite_aqu AS iu ON iu.centre_code = p.centre_code
-                AND iu.unite_numero = p.unite_numero
-                AND iu.auditeur_numero = p.auditeur_numero
-                AND iu.annee = p.annee
-                AND iu.semestre_code = p.semestre_code
-            LEFT JOIN unite_aqu AS u ON u.unite_numero = p.unite_numero
-            LEFT JOIN unite_ouverte_aqu as uo ON uo.centre_code = p.centre_code 
-                AND uo.annee = p.annee 
-                AND uo.unite_numero = p.unite_numero
-                AND uo.semestre_code = p.semestre_code
-                AND uo.groupe_code = iu.groupe_code";
+        $sql = "SELECT    p.*,
+                          i.inscription_date,
+                          i.centre_attachement_libelle,
+                          iu.inscription_unite_date_creation,
+                          iu.modalite,
+                          iu.groupe_code,
+                          iu.sousgroupe_code,
+                          u.unite_code,
+                          u.unite_nb_heure,
+                          u.unite_ects,
+                          uo.regroupement_programme_code,
+                          uo.regroupement_programme_libelle,
+                          i.formation_numero,
+                          i.formation_libelle
+                FROM      prise_en_charge_aqu p
+                LEFT JOIN inscription_aqu i
+                ON        i.centre_code     = 285
+                AND       i.auditeur_numero = p.auditeur_numero
+                AND       i.annee           = p.annee
+                LEFT JOIN inscription_unite_aqu iu
+                ON        iu.centre_code     = i.centre_code
+                AND       iu.unite_numero    = p.unite_numero
+                AND       iu.auditeur_numero = i.auditeur_numero
+                AND       iu.annee           = i.annee
+                AND       iu.semestre_code   = p.semestre_code
+                LEFT JOIN unite_aqu u
+                ON        u.unite_numero = iu.unite_numero
+                LEFT JOIN unite_ouverte_aqu uo
+                ON        uo.centre_code   = iu.centre_code
+                AND       uo.annee         = iu.annee
+                AND       uo.unite_numero  = iu.unite_numero
+                AND       uo.semestre_code = iu.semestre_code
+                AND       uo.groupe_code   = iu.groupe_code";
         if ($filtre->getSql()) {
             $sql .= ' WHERE ' . $filtre->getSql('p.');
         }
